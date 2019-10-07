d51e845145d3d390f53a578ffccfbc3c9b1ab447
jamarton <jamartiton@gmail.com>
2019-10-07 22:12:23 +0200

    lombok

--- demo/src/main/java/com/example/demo/domains/dtos/ActorDTO.java
+++ demo/src/main/java/com/example/demo/domains/dtos/ActorDTO.java
@@ -1,8 +1,10 @@
 package com.example.demo.domains.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
+@Data @NoArgsConstructor @AllArgsConstructor
 public class ActorDTO {
 	private int actorId;
 	private String firstName;
